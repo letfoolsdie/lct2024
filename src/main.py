e5058c5fcc0fd67912b0d4a2489b71bf5c19d298
Nikolay Semyachkin <letfoolsdie@gmail.com>
2024-06-03 16:32:20 +0400

    handle CORS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import engines
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # search_engine = engines.ConstantDB()
 search_engine = engines.ClownDB(
     "data/yappy_hackaton_2024_400k.csv",
