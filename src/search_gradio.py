4daeff69f5cd292c51a5a60d6d7169458ae73e11
timur.abdrashitov <timur.abdrashitov@altezza.org>
2024-06-15 20:53:22 +0300

    rm print

--- src/search_gradio.py
+++ src/search_gradio.py
@@ -3,11 +3,9 @@ import gradio as gr
 
 
 def gradio_search(query: str):
-    print(query)
     video_data = requests.get("http://127.0.0.1:8000/search", params={'query': query}).json()
     html_output = ""
     for vid in video_data:
-        print(video_data)
         video_html = f"""<iframe width="560" height="315" src="{vid['link']}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"""
         html_output += f"{vid['description']}" + "<br>" + video_html + "<br>"
 
