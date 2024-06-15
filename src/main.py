298f810ecbd97b4c7ea482a65007fcc4c34cdb37
timur.abdrashitov <timur.abdrashitov@altezza.org>
2024-06-15 17:44:57 +0300

    update port

--- src/main.py
+++ src/main.py
@@ -11,8 +11,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
+    "http://localhost:8081",
+    "http://127.0.0.1:8001",
     "*"
 ]
 
