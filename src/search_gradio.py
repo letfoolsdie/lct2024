64385fb9ad6661271339468b93d54b0d697d7fe0
Nikolay Semyachkin <letfoolsdie@gmail.com>
2024-06-15 12:37:53 +0000

    update search gradio demo

--- src/search_gradio.py
+++ src/search_gradio.py
@@ -1,27 +1,9 @@
-import engines
+import requests
 import gradio as gr
 
 
-# search_engine = engines.ConstantDB()
-# search_engine = engines.ClownDB(
-#     "data/yappy_hackaton_2024_400k.csv",
-#     "data/embeds_countVectorzer_1-1_word_description.joblib",
-#     "data/countVectorizer_1-1_word.joblib"
-# )
-
-# search_engine = engines.NeuralSearcher(
-#     collection_name="lct_clip",
-#     model_name='distiluse-base-multilingual-cased-v1',
-#     qdrant_url="http://localhost:6333",
-#     device='cuda'
-# )
-
-search_engine = engines.CLIPSearcher(
-    collection_name="lct_clip"
-)
-
 def gradio_search(query: str):
-    video_data = search_engine.search(query)
+    video_data = requests.get("http://127.0.0.1:8000/search", params={'query': query}).json()
     html_output = ""
     for vid in video_data:
         video_html = f"""<iframe width="560" height="315" src="{vid['link']}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"""
@@ -29,10 +11,11 @@ def gradio_search(query: str):
 
     return html_output
 
+
 demo = gr.Interface(
     fn=gradio_search,
     inputs=["text"],
     outputs=["html"],
 )
 
-demo.launch()
+demo.launch(share=True)
